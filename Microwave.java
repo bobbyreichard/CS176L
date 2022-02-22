9042250683ddc150cbfe92e3f29ed0f451e85ec3
bobbyreichard <86379903+bobbyreichard@users.noreply.github.com>
2022-02-22 08:31:31 -0500

    Add files via upload

--- Microwave.java
+++ Microwave.java
@@ -1,21 +1,38 @@
 
 public class Microwave {
 	//Instance Variables
-	int time =0;
-	int power =1;
+	private int time;
+	private int power;
 	
-	//timeButton
-	timeButton = time + 30;
-	
-	//powerButton
-	if (power == 1);
-		power =2;
-		else (power ==2);
+	//Constructor
+	public Microwave()
+	{
+		time = 0;
 		power = 1;
-	
+	}
+	//timeButton
+	public void timeButton() 
+	{
+		time = time + 30;
+	}
+	//powerButton
+	public void powerButton()
+	{	
+	if (power == 1)
+		{power = 2;}
+		else 
+		{power = 1;}
+	}
 	//resetButton
-	
+	public void resetButton()
+	{
+		time = 0;
+		power = 1;
+	}
 		
 	//startButton
-	
+	public void startButton()
+	{
+		System.out.println("Cooking for " + time + " seconds at level " + power + ".");
+	}
 }
--- MicrowaveTester.java
+++ MicrowaveTester.java
@@ -1,7 +1,21 @@
 
 public class MicrowaveTester {
-	public static void main{
 	
+	public static void main(String[] args){
+		//Create Microwave Object
+		Microwave myMicrowave = new Microwave();
+		
+		//Hit Time Button
+		myMicrowave.timeButton();
+		
+		//Hit Power Button
+		myMicrowave.powerButton();
+		
+		//Hit Start Button
+		myMicrowave.startButton();
+		
+		//Hit Reset Button
+		myMicrowave.resetButton();
 		
 	}
 }
